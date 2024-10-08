b438cd5367ec944a949131548c65f95d97d11fd4
Nicole Oliveira <nicole.s.oliveira26@gmail.com>
2024-10-08 12:23:29 +0100

    finished ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsouza-o <nsouza-o@student.42porto.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 20:34:30 by nsouza-o          #+#    #+#             */
+/*   Updated: 2024/10/08 12:22:56 by nsouza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#define RESET       "\033[0m"
+#define RED     	"\033[31m"
+#define BOLD_RED         "\033[1m\033[31m"
+#define BOLD_GREEN       "\033[1m\033[32m"
+#define BOLD_YELLOW      "\033[1m\033[33m"
+#define BOLD_BLUE        "\033[1m\033[34m"
+#define BOLD_WHITE       "\033[1m\033[37m"
+
+class Harl 
+{
+	private:
+		void _debug(void);
+		void _info(void);
+		void _warning(void);
+		void _error(void);
+
+	public:
+		Harl();
+		~Harl();
+		void complain( std::string level );
+};
+
+typedef void (Harl::*HarlMemFn)();
+
+#endif // HARL_HPP
---
+++ ex05/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsouza-o <nsouza-o@student.42porto.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 12:02:55 by nsouza-o          #+#    #+#             */
+/*   Updated: 2024/10/08 12:16:07 by nsouza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+}
